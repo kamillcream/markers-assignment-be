fdc566efbbfb6766cd2bf1c1a7fcc91cc310093c
Jinyoung <glitt5384@naver.com>
2025-09-03 07:42:50 +0900

    chore: cors origin 변경

--- src/main/java/com/markers/global/config/SecurityConfig.java
+++ src/main/java/com/markers/global/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:63342", "http://3.24.136.88"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-CSRF-TOKEN"));
         configuration.addExposedHeader("access");
