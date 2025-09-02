17c1f23fd2fdde9848d788326a409becb6ee01fd
Jinyoung <glitt5384@naver.com>
2025-09-03 07:41:47 +0900

    feat: 회원가입 값 null 여부 확인

--- src/main/java/com/markers/domain/user/dto/RegisterRequest.java
+++ src/main/java/com/markers/domain/user/dto/RegisterRequest.java
@@ -1,4 +1,9 @@
 package com.markers.domain.user.dto;
 
-public record RegisterRequest(String name, String email, String password) {
+import jakarta.validation.constraints.NotNull;
+
+public record RegisterRequest(
+        @NotNull String name,
+        @NotNull String email,
+        @NotNull String password) {
 }
